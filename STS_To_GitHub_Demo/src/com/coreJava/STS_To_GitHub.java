b3eee6bc16462b6e8e556c690d3ba557487b2f90
minuk <minuk@DESKTOP-15R5B74>
2022-01-30 15:25:01 +0530

    My second commit

--- STS_To_GitHub_Demo/src/com/coreJava/STS_To_GitHub.java
+++ STS_To_GitHub_Demo/src/com/coreJava/STS_To_GitHub.java
@@ -4,6 +4,6 @@ package com.coreJava;
 
 public class STS_To_GitHub {
 	public static void main(String[] args) {
-		System.out.println("Helloooo....");
+		System.out.println("Helloooo alll....");
 	}
 }
